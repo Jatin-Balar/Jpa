c201a1e502862d9e1d503017b25e8ed4d050168a
Sara Patel <jatinbalar348@gmail.com>
2023-11-28 15:16:49 -0500

    first commit

--- src/main/java/com/jatin/Jpa/Controller.java
+++ src/main/java/com/jatin/Jpa/Controller.java
@@ -1,22 +1,28 @@
 package com.jatin.Jpa;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
 	
-	@Autowired
-	UserDao userDao;
+	UserRepo repo;
+	
+
+	public Controller(UserRepo repo) {
+		this.repo = repo;
+	}
+
 
 	@GetMapping("/")
 	public String hello() {
 		return "hello world..................";
 	}
+	
 	@GetMapping("/hello")
 	public User helloWorld() {
-		return userDao.findById(1).get();
+		return repo.findById(1).get();
 	}
+
 
 }
--- src/main/java/com/jatin/Jpa/UserRepo.java
+++ src/main/java/com/jatin/Jpa/UserRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Integer> {
+public interface UserRepo extends JpaRepository<User, Integer> {
 
 }
